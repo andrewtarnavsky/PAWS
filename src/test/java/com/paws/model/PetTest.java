0e0ecd05719cf261e9001f0a45dc1fab9a0a733a
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2025-10-03 17:54:48 -0400

    Added JUnit Tests to file structure

---
+++ src/test/java/com/paws/model/PetTest.java
@@ -0,0 +1,14 @@
+package com.paws.model;
+
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.Period;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PetTest {
+    @Test
+    void computesAgeCorrectly(){
+
+    }
+}
