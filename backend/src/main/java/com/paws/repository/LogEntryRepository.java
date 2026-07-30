345df20f1239afa0a0d1d55871fb5b610245a59e
andrewtarnavsky <andrew.tarnavsky@gmail.com>
2026-07-30 16:25:26 -0400

    Add findById method to LogRepository

---
+++ backend/src/main/java/com/paws/repository/LogEntryRepository.java
@@ -0,0 +1,37 @@
+package com.paws.repository;
+
+import com.paws.model.LogEntry;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public class LogEntryRepository {
+    private JdbcTemplate db;
+
+    public LogEntryRepository(JdbcTemplate db){
+        this.db = db;
+    }
+
+    public LogEntryRepository(){
+
+    }
+
+    public Optional<LogEntry> findById(Long id){
+        String sql = """
+                SELECT * FROM log_entries WHERE id = ?
+                """;
+        try {
+            LogEntry logEntry = db.queryForObject(sql, new BeanPropertyRowMapper<>(LogEntry.class), id);
+            return Optional.of(logEntry);
+        } catch (EmptyResultDataAccessException exception) {
+            return Optional.empty();
+        }
+
+
+    }
+}
